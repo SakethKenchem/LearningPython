0a283e948609bfddad12c732771cb07afd7cb33c
SakethKenchem <s.kenchem@gmail.com>
2023-11-26 11:39:59 +0300

    third commit

--- if_statements.py
+++ if_statements.py
@@ -1,7 +1,7 @@
 age = int(input("age = "))
 
 if age == 100:
-    print("Woah too old man. get better things to do")
+    print("Woah too old man. get better things to do!")
 elif age >= 18:
     print(age,"is old enough")
 elif age < 0:
