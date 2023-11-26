05502a1bd106d67152915d2d19ddbae1522df505
SakethKenchem <s.kenchem@gmail.com>
2023-11-26 11:42:05 +0300

    third commit

--- logicalOperators.py
+++ logicalOperators.py
@@ -5,4 +5,4 @@ if temperature >= 0 and temperature <= 30:
 elif temperature >= 31 and temperature <=39:
     print("don't go out!")
 elif not temperature == 25:
-    print("wear something warm")
+    print("wear something warm!")
